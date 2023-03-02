08591cf55cf7d3206cc3f92e4ef9e895b9389daa
“Simona <simona.ignacz@gmail.com>
2023-03-02 18:17:04 +0100

    uprava

--- main.js
+++ main.js
@@ -10,16 +10,37 @@
 
 parseDate("01.01.2023") */
 
-const date = ({ day: 6, month: 4, year: 2021 })
+
+
+
+/* const date = ({ day: 6, month: 4, year: 2021 })
 
 const formatDate = (date) => {
-    return {
-        let day = date.day,
-        month: date.padStart(3, 5),
-        year: date.padStart(6)
-    }
+    let day = date.day
+    let month = date.month
+    let year = date.year
+
+    return day + "." + month + "." + "year"
+
 }
 
+
+
+let myDate = 
+ */
 /* formatDate({ day: 6, month: 4, year: 2021 }) */
 
 
+
+/* const round = (myNumber) => {
+    const a = Math.trunc(myNumber)
+    const b = myNumber - a
+    if (Math.abs(b) === 0.5) {
+        if (a % 2 === 0) {
+            return a
+        } else {
+            return a + 2 * b
+        }
+    }
+    return Math.round(myNumber)
+} */
